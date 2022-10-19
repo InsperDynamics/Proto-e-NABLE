f1e1d3a19e44f8806868e0747dbacd92180896e7
Peng1104 <admin@peng1104.net>
2022-10-19 18:40:58 -0300

    BugFix

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,20 @@
 int delayTime = 24;              // The delay time between each step in milliseconds max (45)
 int updateList[] = {0, 2, 4, 6}; // Array containing the servos that should be updated in each loop (0-7)
 
+int minAngle = 40;  // The minimum angle of the servo
+int maxAngle = 170; // The maximum angle of the servo
+
 Servo servos[] = {Servo(), Servo(), Servo(), Servo(), Servo(), Servo(), Servo(), Servo()};
 
 uint8_t step[] = {4, 4, 4, 4, 4, 4, 4, 4}; // The step value for each servo
 
 void setup()
 {
-  int pins[] = {5, 6, 7, 8, 9, 10, 11, 12}; // The pins used for the servos
-  int startAngles = {90, 130, 90, 100, 90, 90, 90, 90}; // The start angles for each servo
+  int pins[] = {5, 6, 7, 8, 9, 10, 11, 12};               // The pins used for the servos
+  int startAngles[] = {90, 130, 90, 100, 90, 90, 90, 90}; // The start angles for each servo
 
   for (int i = 0; i < 8; i++)
-  
+
   {
     servos[i].attach(pins[i]);
     servos[i].write(startAngles[i]);
@@ -25,9 +28,11 @@ void loop()
 {
   for (int position : updateList)
   {
-    servos[position].write(servos[position].read() + step[position]); // Update the servo position to the new angle
+    int angle = servos[position].read() + step[position];
 
-    if (servos[position].changeDirection()) // Check if step need to be inverted
+    servos[position].write(angle); // Update the servo position to the new angle
+
+    if (angle < minAngle || angle > maxAngle) // Check if step need to be inverted
     {
       step[position] = -step[position]; // Reverse the step value
     }
