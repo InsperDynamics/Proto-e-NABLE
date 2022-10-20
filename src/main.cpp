8c4c0ed493c7ee59b819f272a7cf903204a7ce44
Peng1104 <admin@peng1104.net>
2022-10-20 11:03:23 -0300

    Documentation Fix

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,11 @@
 
 // ####################################### START OF CONFIGURATION #######################################
 
-// Each servo and its start angle 
+// Each servo and its start angle
 // Format: {ping, angle}
 int pins[][2] = {{5, 90}, {6, 130}, {7, 90}, {8, 100}, {9, 90}, {10, 90}, {11, 90}, {12, 90}};
 
-// Array containing all servos that move, using the step values (0 - (size -1))
+// Array containing all servos that move, using the step values 0 to (size -1)
 int updateList[] = {0, 2, 4, 6};
 
 // The step value for each servo, the amount of degrees it should move each loop
