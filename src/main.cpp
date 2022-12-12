7461660b1016265a22179773b27e5f71607d4797
Vacbo <pedrobp1@al.insper.edu.br>
2022-12-12 16:23:53 -0300

    Mudanca final para exposicao

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,13 @@
 // Nano
 //int pins[][2] = {{PIN2, 90}, {PIN3, 130}, {PIN4, 90}, {PIN5, 100}};
 // Mega
-int pins[][2] = {{PIN0, 90}, {PIN1, 90}, {PIN2, 90}, {PIN3, 90}, {PIN4, 90}, {PIN5, 90}, {PIN6, 90}, {PIN7, 90}};
+int pins[][2] = {{PIN0, 0}, {PIN1, 90}, {PIN2, 130}, {PIN3, 90}, {PIN4, 100}, {PIN5, 90}, {PIN6, 180}, {PIN7, 0}};
 
 // Array containing all servos that move, using the step values 0 to (size -1)
-int updateList[] = {0, 2, 4, 6};
+int updateList[] = {0,2,4,6};
 
 // The step value for each servo, the amount of degrees it should move each loop
-int step[] = {1, 1, 1, 1, 1, 1, 1, 1};
+int step[] = {-1, 1, 1, 1, 1, 1, -1, 1};
 
 // The delay time between each step in milliseconds
 int delayTime = 62;
@@ -25,7 +25,7 @@ int delayTime = 62;
 int minAngle = 40;
 
 // The maximum angle of the servo
-int maxAngle = 170;
+int maxAngle = 140;
 
 // ####################################### END OF CONFIGURATION #######################################
 
